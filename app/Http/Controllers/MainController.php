76ae9b31a32d5e53cde09795ca58f6e20ec13a97
Max Ferreira <max.alexxandre@gmail.com>
2026-07-01 21:27:55 -0300

    Criando Rotas, Controllers e View já com o Layout.

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class MainController extends Controller
+{
+    public function home(): View
+    {
+        return view('home');
+    }
+    public function generateExercises(Request $request)
+    {
+        echo 'gerar exercícios';
+    }
+
+    public function printExercises()
+    {
+        echo 'Imprimir exercícios no navegador';
+    }
+
+    public function exportExercises()
+    {
+        echo 'exportar exercícios para um arquivo de texto';
+    }
+}
