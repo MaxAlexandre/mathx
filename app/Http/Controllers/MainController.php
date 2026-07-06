781cf11d7125c3d2cf58fad3f00e88eacde8272c
Max Ferreira <max.alexxandre@gmail.com>
2026-07-06 20:40:54 -0300

    Criando lógica das operações matemáticas e sua view com os dados dinâmicos.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,19 +11,97 @@ class MainController extends Controller
     {
         return view('home');
     }
-    public function generateExercises(Request $request)
+
+    public function generateExercises(Request $request) : View
     {
         //form validation
 
         $request->validate([
-            'check_sum' =>'required_without_all:check_subtraction,check_multiplication,check_division',
-            'check_subtraction' =>'required_without_all:check_sum,check_multiplication,check_division',
-            'check_multiplication' =>'required_without_all:check_sum,check_subtraction,check_division',
-            'check_division' =>'required_without_all:check_sum,check_subtraction,check_multiplication',
-            'number_one' => 'required|integer|min:0|max:999',
+            'check_sum' => 'required_without_all:check_subtraction,check_multiplication,check_division',
+            'check_subtraction' => 'required_without_all:check_sum,check_multiplication,check_division',
+            'check_multiplication' => 'required_without_all:check_sum,check_subtraction,check_division',
+            'check_division' => 'required_without_all:check_sum,check_subtraction,check_multiplication',
+            'number_one' => 'required|integer|min:0|max:999|lt:number_two',
             'number_two' => 'required|integer|min:0|max:999',
             'number_exercises' => 'required|integer|min:5|max:50',
         ]);
+
+        // get selected operations
+
+        $operations = [];
+        if ($request->check_sum) {
+            $operations[] = 'sum';
+        }
+        if ($request->check_subtraction) {
+            $operations[] = 'subtraction';
+        }
+        if ($request->check_multiplication) {
+            $operations[] = 'multiplication';
+        }
+        if ($request->check_division) {
+            $operations[] = 'division';
+        }
+
+        //get numbers (min and max)
+        $min = $request->number_one;
+        $max = $request->number_two;
+
+        //get number of exercises
+        $numberExercises = $request->number_exercises;
+
+        //generate exercises
+        $exercises = [];
+        for ($index = 1; $index <= $numberExercises; $index++) {
+
+            $operation = $operations[array_rand($operations)];
+            $number1 = rand($min, $max);
+            $number2 = rand($min, $max);
+
+            $exercise = '';
+            $sollution = '';
+
+            switch ($operation) {
+                case 'sum':
+                    $exercise = "$number1 + $number2 =";
+                    $sollution = $number1 + $number2;
+                    break;
+                case 'subtraction':
+                    $exercise = "$number1 - $number2 =";
+                    $sollution = $number1 - $number2;
+                case 'multiplication':
+                    $exercise = "$number1 x $number2 =";
+                    $sollution = $number1 * $number2;
+                    break;
+                case 'division':
+
+                    // avoid division by zero
+                    if ($number2 == 0) {
+                        $number2 = 1;
+                    }
+
+                    $exercise = "$number1 : $number2 =";
+                    $sollution = $number1 / $number2;
+                    break;
+
+            }
+
+            //if $sollution is a float number, round it to 2 decimal places
+            if (is_float($sollution)) {
+                $sollution = round($sollution, 2);
+            }
+
+            $exercises[] = [
+                'operation' => $operation,
+                'exercise_number' => $index,
+                'exercise' => $exercise,
+                'sollution' => "$exercise $sollution"
+            ];
+
+        }
+
+        return view('operations', ['exercises' => $exercises]);
+
+
     }
 
     public function printExercises()
